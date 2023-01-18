4058822b8b01a1929cba6471e8629d1a76311d8e
ysksd <shoh.7514@gmail.com>
2023-01-18 17:51:23 +0900

    CRUD機能のテスト

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\InertiaTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,16 @@ use Inertia\Inertia;
 Route::get('/inertia-test', function () {
     return Inertia::render('InertiaTest');
 });
+
+Route::get('/component-test', function () {
+    return Inertia::render('ComponentTest');
+});
+
+Route::get('/inertia/index', [InertiaTestController::class, 'index'])->name('inertia.index');
+Route::get('/inertia/create', [InertiaTestController::class, 'create'])->name('inertia.create');
+Route::post('/inertia', [InertiaTestController::class, 'store'])->name('inertia.store');
+Route::get('/inertia/show/{id}', [InertiaTestController::class, 'show'])->name('inertia.show');
+Route::delete('/inertia/{id}', [InertiaTestController::class, 'delete'])->name('inertia.delete');
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
