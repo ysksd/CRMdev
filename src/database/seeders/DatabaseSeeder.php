479cd88dd94307dd0d5267061ba08abd2ca5f4b3
ysksd <shoh.7514@gmail.com>
2023-02-25 19:00:14 +0900

    購入画面作成、機能実装

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\Customer;
+use \App\Models\Item;
+use \App\Models\Purchase;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,7 +22,17 @@ class DatabaseSeeder extends Seeder
             ItemSeeder::class,
         ]);
 
-        \App\Models\Customer::factory(1000)->create();
+        Customer::factory(1000)->create();
+
+        $items = Item::all();
+
+        Purchase::factory(100)->create()
+        ->each(function(Purchase $purchase) use ($items) {
+            $purchase->items()->attach(
+                $items->random(rand(1,3))->pluck('id')->toArray(),
+                ['quantity' => rand(1, 5)]
+            );
+        });
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
