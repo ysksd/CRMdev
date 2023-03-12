bb803a6477fa6a0068f80797e20f600ad25c3f9b
ysksd <shoh.7514@gmail.com>
2023-03-12 17:39:42 +0900

    vue-chart-3インポート

--- src/routes/web.php
+++ src/routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\InertiaTestController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\AnalysisController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,7 @@ use App\Http\Controllers\PurchaseController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('analysis', [AnalysisController::class, 'index'])->name('analysis');
 
 Route::resource('items', ItemController::class)
 ->middleware(['auth', 'verified']);
